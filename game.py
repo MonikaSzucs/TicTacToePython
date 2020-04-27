962ec65d3b47c8006eae60a8df7604eda7a9a449
Monika Szucs <monika.silvia.s@hotmail.ca>
2020-04-26 20:59:41 -0700

    Comments and For Loop
    Added multiline and single line comments.
    
    Created a for loops to count rows and display the matrix as it loops through.

--- game.py
+++ game.py
@@ -1,6 +1,16 @@
+''' Tic Tac Toe Game
+    By Monika Szucs
+    Started: April 26, 2020
+'''
+
+#matrix
 game = [[0, 0, 0],
        [0, 0, 0],
        [0, 0, 0]]
+       
+#print top row numbers
+print("   a  b  c")
 
-for row in game:
-    print(row)
+#loop through matrix and count left column
+for count, row in enumerate(game):
+    print(count, row)
