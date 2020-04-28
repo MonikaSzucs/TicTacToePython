77462d8183758a580498134db6d81822427f413a
Monika Szucs <monika.silvia.s@hotmail.ca>
2020-04-28 16:39:55 -0700

    Added a Quiz
    Added a quiz to test myself if something should be global in order to save a variable

--- game.py
+++ game.py
@@ -20,6 +20,7 @@ def game_board(player=0, row=0, column=0, just_display=False):
     for count, row in enumerate(game):
         print(count, row)
 
-
+print(game)
 game_board(just_display=True)
 game_board(player=1, row=1, column=1)
+print(game)
