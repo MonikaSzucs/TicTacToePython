ef02971e216ec12b0c4adc75ec0ded67393a516c
Monika Szucs <monika.silvia.s@hotmail.ca>
2020-04-28 21:38:58 -0700

    Done | MS

--- testingground.py
+++ testingground.py
@@ -19,3 +19,21 @@ dat()
 
 from moddir.examplemod2 import do_a_thing
 do_a_thing()
+
+#google python package index and look through the website to find what you want to install
+
+
+'''
+Adding in Colorama Package
+'''
+
+'''
+from colorama import Fore, Back, Style, init
+init()
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now
+'''
