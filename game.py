68448a802b49737e986c1f1716b21b44e9c2ecc2
Monika Szucs <monika.silvia.s@hotmail.ca>
2020-04-28 17:26:42 -0700

    Error Handling
    Worked on finding and eliminating errors

--- game.py
+++ game.py
@@ -10,17 +10,39 @@ game = [[0, 0, 0],
 
 
 #function
-def game_board(player=0, row=0, column=0, just_display=False):
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
     #print top row numbers
-    print("   a  b  c")
-    if not just_display:
-        game[row][column] = player
+    try:
+        print("   a  b  c")
+        if not just_display:
+            game_map[row][column] = player
 
-    #loop through matrix
-    for count, row in enumerate(game):
-        print(count, row)
+        #loop through matrix
+        for count, row in enumerate(game_map):
+            print(count, row)
+        #raise can also be added before return
+        return game_map
+    except IndexError as e:
+            print("Error: make sure you input row/column as 0 1 or 2?", e)
+    except Exception as e:
+        print("Something went very wrong!", e)
+
+'''
+this are some other things you can add but not needed right now
+    else:
+
+    finally:
+'''
 
 print(game)
-game_board(just_display=True)
-game_board(player=1, row=1, column=1)
+game = game_board(game, just_display=True)
+game = game_board(game_board, player=1, row=3, column=1)
 print(game)
+
+'''
+
+open up cmd
+type in cd ot nevigate to folder
+the type: python game.py
+
+'''
